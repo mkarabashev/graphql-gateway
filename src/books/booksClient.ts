506797ef3229f6488597043a3608d8b4ec28d0bf
Maksim Karabashev <mkarabashev@gmail.com>
2019-04-16 12:30:19 +0200

    added typings for the book client

--- src/books/booksClient.ts
+++ src/books/booksClient.ts
@@ -1,13 +1,14 @@
 import * as fetch from 'isomorphic-fetch'
+import { Book } from './Book'
 import { PORT } from './bookApp'
 
 const serializeIds = (ids: number[]): string => ids.join(',')
 
 export const bookClient = {
-    getBook: async (id: number) =>
+    getBook: async (id: number): Promise<Book> =>
         fetch(`http://localhost:${PORT}/books/${id}`)
             .then(res => res.json()),
-    findBooks: (ids: number[]) =>
+    findBooks: (ids: number[]): Promise<Book[]> =>
         fetch(`http://localhost:${PORT}/books?ids=${serializeIds(ids)}`)
             .then(res => res.json())
 }
